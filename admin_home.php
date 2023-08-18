9a4e2561a767539a93d3563ffbbf4f7f228b86ca
crystal_qazi <khalil.azeem6@gmail.com>
2023-08-18 16:31:51 +0500

    admin panel

---
+++ admin_home.php
@@ -0,0 +1,24 @@
+<?php include('headerfootertemp/admin/adminheader.php') ?>
+
+
+                        <!--================Topic Area =================-->
+    
+                <div class="col-lg-7 col-md-8 doc-middle-content">
+                    <div id="post" class="shortcode_info">
+                        
+
+                        
+
+                        
+
+                        </div>
+                    </div>                  
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--================End Topic Area =================-->
+
+    
+    <?php include('HeaderFooterTemp\admin\adminfooter.php') ?>
+
