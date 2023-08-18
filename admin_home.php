4686c8328944e91a2eb596fc11485449ba13b756
crystal_qazi <khalil.azeem6@gmail.com>
2023-08-18 16:00:07 +0500

    admin pannel

---
+++ admin_home.php
@@ -0,0 +1,31 @@
+<?php ini_set('display_errors', 0); ?>
+<?php include('headerfootertemp/admin/admin_head.php') ?>
+<?php
+// Include the database configuration
+include 'config/connection.php'; ?>
+
+<?php
+// Include the database configuration
+include 'config/connection.php';?>
+
+             
+              
+                    
+
+                    <div class="col-lg-7 col-md-8 doc-middle-content">
+                        <div id="post" class="shortcode_info">
+
+                        </div>
+                    </div>                    
+                    
+                      
+
+                    
+          
+               
+                </div>
+            </div>
+        </section>
+        <!--================End Topic Area =================-->
+
+        <?php include('headerfootertemp/admin/admin_footer.php') ?>
