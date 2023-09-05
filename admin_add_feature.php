60c429aca14ea03e5258da105b12bbc583f0c9f8
shezi <111431490+ShazorKhan06@users.noreply.github.com>
2023-09-05 15:43:10 +0500

    worked on feature

--- admin_add_feature.php
+++ admin_add_feature.php
@@ -17,38 +17,163 @@
         <div class="row">
             <div class="col-12 d-flex">
                 <div class="col-3">
-                    <label for="main-menu">Select Main Menu</label>
+                  <?php
+                     include 'config/connection.php';
+
+                    $sql = "SELECT * FROM module";
+                    $res = mysqli_query($con,$sql);
+                    $count = mysqli_num_rows($res);
+
+                  ?>
+                    <label for="module">Select Main Menu</label>
                     <select class="form-select form-select-sm form-control" id="main-menu" aria-label="Default select example">
-                        <option selected>Open this select menu</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
+                    <option selected>Select Any</option>
+                        
+                        <?php
+                          if($count > 0){
+                              while($row= mysqli_fetch_assoc($res)){
+                                  $id = $row['ID'];
+                                  $Module_name = $row['Module_name'];
+                                  ?>
+                                  <option class="nav-item" value="<?php echo $id; ?>"><?php echo $Module_name; ?></option>
+                                  <?php                                     
+                              }
+                          }
+                        ?>
                       </select>
                 </div>
                 <div class="col-3">
+                <?php
+                     include 'config/connection.php';
+
+                    $sql = "SELECT * FROM project";
+                    $res = mysqli_query($con,$sql);
+                    $count = mysqli_num_rows($res);
+
+                  ?>
                     <label for="main-menu">Select Main Menu</label>
                     <select class="form-select form-select-sm form-control" aria-label="Default select example">
-                        <option selected>Open this select menu</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
+                    <option selected>Select Any</option>
+                        
+                        <?php
+                          if($count > 0){
+                              while($row= mysqli_fetch_assoc($res)){
+                                  $id = $row['ID'];
+                                  $Project_name = $row['Project_name'];
+                                  ?>
+                                  <option class="nav-item" value="<?php echo $id; ?>"><?php echo $Project_name; ?></option>
+                                  <?php                                     
+                              }
+                          }
+                        ?>
+
                       </select>
                 </div>
                 <div class="col-3">
+                <?php
+                     include 'config/connection.php';
+
+                    $sql = "SELECT * FROM main_menu";
+                    $res = mysqli_query($con,$sql);
+                    $count = mysqli_num_rows($res);
+
+                  ?>
                     <label for="main-menu">Select Main Menu</label>
                     <select class="form-select form-select-sm form-control" aria-label="Default select example">
-                        <option selected>Open this select menu</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
+                    <option selected>Select Any</option>
+                        
+                        <?php
+                          if($count > 0){
+                              while($row= mysqli_fetch_assoc($res)){
+                                  $id = $row['id'];
+                                  $title = $row['title'];
+                                  ?>
+                                  <option class="nav-item" value="<?php echo $id; ?>"><?php echo $title; ?></option>
+                                  <?php                                     
+                              }
+                          }
+                        ?>
                       </select>
                 </div>
 
                 
             </div>
+            
         </div>
+        
       </form>
-    </div>
+      
+</div>
+    <form  method="post">
+
+        <div class="mb-3">
+            <label class="form-label">Add Feature</label>
+            <input name="title" class="form-control form-control-sm" type="text"
+                placeholder="Name Of Feature" aria-label=".form-control-sm example">
+        </div>
+        <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Description</label>
+            <textarea type="description" name="description" class="form-control" rows="3"
+                placeholder="Describe Feature"></textarea>
+        </div>
+        <div class="col-6 d-flex">
+        <div class="col-3">
+                  <?php
+                     include 'config/connection.php';
+
+                    $sql = "SELECT * FROM tags";
+                    $res = mysqli_query($con,$sql);
+                    $count = mysqli_num_rows($res);
+
+                  ?>
+                    
+                    <select class="form-select form-select-sm form-control" id="main-menu" aria-label="Default select example">
+                    <option selected>Tag</option>
+                        
+                        <?php
+                          if($count > 0){
+                              while($row= mysqli_fetch_assoc($res)){
+                                  $id = $row['ID'];
+                                  $Tag_name = $row['Tag_name'];
+                                  ?>
+                                  <option class="nav-item" value="<?php echo $id; ?>"><?php echo $Tag_name; ?></option>
+                                  <?php                                     
+                              }
+                          }
+                        ?>
+                      </select>
+                </div>
+                
+                <div class="col-3">
+                <button type="submit" class="btn btn-primary" name="submit" value="submit">Submit</button>
+
+                </div>
+              </div>
+    </form>
+    <?php
+        if(isset($_POST['submit'])){
+    
+            //include 'config/connection.php';
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            
+                // collect value of input field
+                $Subject_name= $_POST['Subject_name'];
+                $description=$_POST['Description'];
+                $Module_id=$_POST['Module_id'];
+                $project_id=$_POST['project_id'];
+                $main_menu_id=$_POST['main_menu_id'];
+                $Tagged_id=$_POST['Tagged_id'];
+            
+                if (empty($title)) {
+                    echo "data is empty";
+                } else {
+                    $sql= "INSERT into subject (Subject_name, Description,Module_id,project_id,main_menu_id,Tagged_id) VALUES ('$Subject_name','$description','$Module_id','$project_id','$main_menu_id','$Tagged_id')";
+                    $result= mysqli_query($con,$sql);
+                }
+            }}
+            // Closing the connection.
+            $con->close();
+      ?>
 </div>
 
 </div>
--- k_b.sql
+++ k_b.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Sep 01, 2023 at 10:27 AM
+-- Generation Time: Sep 05, 2023 at 05:59 AM
 -- Server version: 8.0.30
 -- PHP Version: 7.4.33
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `k_b`
 --
+CREATE DATABASE IF NOT EXISTS `k_b` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `k_b`;
 
 -- --------------------------------------------------------
 
@@ -27,20 +29,22 @@ SET time_zone = "+00:00";
 -- Table structure for table `main_menu`
 --
 
-CREATE TABLE `main_menu` (
-  `id` int NOT NULL,
+DROP TABLE IF EXISTS `main_menu`;
+CREATE TABLE IF NOT EXISTS `main_menu` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `title` varchar(250) COLLATE utf8mb4_general_ci NOT NULL,
   `description` text COLLATE utf8mb4_general_ci,
-  `dummy` text COLLATE utf8mb4_general_ci
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `dummy` text COLLATE utf8mb4_general_ci,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `main_menu`
 --
 
 INSERT INTO `main_menu` (`id`, `title`, `description`, `dummy`) VALUES
-(1, 'Blog', NULL, NULL),
-(2, 'Forum', NULL, NULL),
+(1, 'Home', '', NULL),
+(2, 'Projects', '', NULL),
 (3, 'Help', NULL, NULL);
 
 -- --------------------------------------------------------
@@ -49,32 +53,27 @@ INSERT INTO `main_menu` (`id`, `title`, `description`, `dummy`) VALUES
 -- Table structure for table `module`
 --
 
-CREATE TABLE `module` (
-  `ID` int NOT NULL,
+DROP TABLE IF EXISTS `module`;
+CREATE TABLE IF NOT EXISTS `module` (
+  `ID` int NOT NULL AUTO_INCREMENT,
   `UID` int DEFAULT NULL,
   `Module_name` varchar(250) COLLATE utf8mb4_general_ci NOT NULL,
   `Description` text COLLATE utf8mb4_general_ci NOT NULL,
   `Date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `project_id` int NOT NULL,
-  `Dummy2` int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `Dummy2` int DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `UID` (`UID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `module`
 --
 
 INSERT INTO `module` (`ID`, `UID`, `Module_name`, `Description`, `Date`, `project_id`, `Dummy2`) VALUES
-(1, 100000, 'My Prescription', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '2023-08-12 00:00:00', 1, 1),
-(2, NULL, 'Manage Patients', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '2023-08-12 00:00:00', 1, NULL),
-(3, NULL, 'Drug information', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '2023-08-12 00:00:00', 1, NULL),
-(4, NULL, 'Pharmacy Service', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '2023-08-12 00:00:00', 1, NULL),
-(5, NULL, 'test', 'test', '2023-08-30 00:00:00', 1, NULL),
-(6, NULL, 'trtr', 'rtrt', '2023-08-31 16:30:19', 2, NULL),
-(7, NULL, 'aazee', 'sdf', '2023-09-01 15:20:58', 1, NULL),
-(8, NULL, 'sdfsdf', 'sdfsdf', '2023-09-01 15:21:06', 2, NULL),
-(9, NULL, 'test2', 'asdf', '2023-09-01 15:24:00', 2, NULL),
-(10, NULL, 'asf', 'asdf', '2023-09-01 15:24:51', 2, NULL),
-(11, NULL, 'asdf', 'asdfasf', '2023-09-01 15:25:58', 1, NULL);
+(1, NULL, 'New Order', 'tt', '2023-09-01 17:55:02', 25, NULL),
+(2, NULL, 'All Orders', 'test', '2023-09-01 17:55:27', 25, NULL),
+(3, NULL, 'Reports', 'tt', '2023-09-05 10:22:41', 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -82,28 +81,26 @@ INSERT INTO `module` (`ID`, `UID`, `Module_name`, `Description`, `Date`, `projec
 -- Table structure for table `project`
 --
 
-CREATE TABLE `project` (
-  `ID` int NOT NULL,
+DROP TABLE IF EXISTS `project`;
+CREATE TABLE IF NOT EXISTS `project` (
+  `ID` int NOT NULL AUTO_INCREMENT,
   `UID` int DEFAULT NULL,
   `Project_name` varchar(250) COLLATE utf8mb4_general_ci NOT NULL,
   `Description` text COLLATE utf8mb4_general_ci NOT NULL,
   `Date` datetime DEFAULT CURRENT_TIMESTAMP,
   `main_menu_id` int DEFAULT NULL,
   `Dummy3` int DEFAULT NULL,
-  `Dummy4` int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `Dummy4` int DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `main_menu_id` (`main_menu_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `project`
 --
 
 INSERT INTO `project` (`ID`, `UID`, `Project_name`, `Description`, `Date`, `main_menu_id`, `Dummy3`, `Dummy4`) VALUES
-(1, 100000000, 'Pharmacy', 'fgfg', '2023-08-12 00:00:00', 2, 1, 1),
-(2, NULL, 'Store', 'dfgdf', '2023-08-12 00:00:00', 2, 1, 1),
-(3, NULL, 'Fast_food', 'dfgdg', '2023-08-12 00:00:00', 3, NULL, NULL),
-(7, 12346, 'Medical Store', 'dsdsvdv', '2023-08-30 00:00:00', 1, NULL, NULL),
-(22, NULL, 'tet', 'test', '2023-08-31 16:24:44', 1, NULL, NULL),
-(23, NULL, 'aasdf', 'asfd', '2023-09-01 15:26:08', 3, NULL, NULL);
+(1, NULL, 'Pharmacy', 'test', '2023-09-01 17:54:16', 1, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -111,27 +108,31 @@ INSERT INTO `project` (`ID`, `UID`, `Project_name`, `Description`, `Date`, `main
 -- Table structure for table `subject`
 --
 
-CREATE TABLE `subject` (
-  `ID` int NOT NULL,
+DROP TABLE IF EXISTS `subject`;
+CREATE TABLE IF NOT EXISTS `subject` (
+  `ID` int NOT NULL AUTO_INCREMENT,
   `UID` int DEFAULT NULL,
   `Subject_name` varchar(250) COLLATE utf8mb4_general_ci NOT NULL,
   `Description` text COLLATE utf8mb4_general_ci NOT NULL,
   `Date` date NOT NULL,
   `Module_id` int DEFAULT NULL,
   `Tagged_id` varchar(250) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `Column 8` int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `project_id` int DEFAULT NULL,
+  `main_menu_id` int NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `UID` (`UID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `subject`
 --
 
-INSERT INTO `subject` (`ID`, `UID`, `Subject_name`, `Description`, `Date`, `Module_id`, `Tagged_id`, `Column 8`) VALUES
-(1, 100000, 'Add A Prescription', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '2023-08-12', 1, '1', NULL),
-(2, 100001, 'Cancel Prescription', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '2023-08-12', 1, '1', NULL),
-(3, 100002, 'Delete Patient', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '2023-08-12', 2, '1', NULL),
-(4, NULL, 'Reports', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '2023-08-12', 1, '2', NULL),
-(5, NULL, 'Reports', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '2023-08-16', 3, '2', NULL);
+INSERT INTO `subject` (`ID`, `UID`, `Subject_name`, `Description`, `Date`, `Module_id`, `Tagged_id`, `project_id`, `main_menu_id`) VALUES
+(1, 100000, 'Add A Prescription', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '2023-08-12', 1, '1', 1, 1),
+(2, 100001, 'Cancel Prescription', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '2023-08-12', 1, '1', 1, 1),
+(3, 100002, 'Delete Patient', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '2023-08-12', 2, '1', 1, 1),
+(4, NULL, 'Reports', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '2023-08-12', 1, '2', 1, 1),
+(5, NULL, 'Reports', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '2023-08-16', 3, '2', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -139,12 +140,14 @@ INSERT INTO `subject` (`ID`, `UID`, `Subject_name`, `Description`, `Date`, `Modu
 -- Table structure for table `sub_menu`
 --
 
-CREATE TABLE `sub_menu` (
-  `id` int NOT NULL,
+DROP TABLE IF EXISTS `sub_menu`;
+CREATE TABLE IF NOT EXISTS `sub_menu` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `title` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `description` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `dummy1` varchar(255) COLLATE utf8mb4_general_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `dummy1` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `sub_menu`
@@ -163,12 +166,15 @@ INSERT INTO `sub_menu` (`id`, `title`, `description`, `dummy1`) VALUES
 -- Table structure for table `tags`
 --
 
-CREATE TABLE `tags` (
-  `ID` int NOT NULL,
+DROP TABLE IF EXISTS `tags`;
+CREATE TABLE IF NOT EXISTS `tags` (
+  `ID` int NOT NULL AUTO_INCREMENT,
   `UID` int NOT NULL,
   `Tag_name` varchar(250) COLLATE utf8mb4_general_ci NOT NULL,
-  `Description` text COLLATE utf8mb4_general_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `Description` text COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `UID` (`UID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tags`
@@ -177,90 +183,6 @@ CREATE TABLE `tags` (
 INSERT INTO `tags` (`ID`, `UID`, `Tag_name`, `Description`) VALUES
 (1, 10001, 'Order', 'Oder Detail'),
 (2, 10002, 'Reports', 'Reports Detail');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `main_menu`
---
-ALTER TABLE `main_menu`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `module`
---
-ALTER TABLE `module`
-  ADD PRIMARY KEY (`ID`),
-  ADD UNIQUE KEY `UID` (`UID`);
-
---
--- Indexes for table `project`
---
-ALTER TABLE `project`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `main_menu_id` (`main_menu_id`);
-
---
--- Indexes for table `subject`
---
-ALTER TABLE `subject`
-  ADD PRIMARY KEY (`ID`),
-  ADD UNIQUE KEY `UID` (`UID`);
-
---
--- Indexes for table `sub_menu`
---
-ALTER TABLE `sub_menu`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `tags`
---
-ALTER TABLE `tags`
-  ADD PRIMARY KEY (`ID`),
-  ADD UNIQUE KEY `UID` (`UID`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `main_menu`
---
-ALTER TABLE `main_menu`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
-
---
--- AUTO_INCREMENT for table `module`
---
-ALTER TABLE `module`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
---
--- AUTO_INCREMENT for table `project`
---
-ALTER TABLE `project`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
-
---
--- AUTO_INCREMENT for table `subject`
---
-ALTER TABLE `subject`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `sub_menu`
---
-ALTER TABLE `sub_menu`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `tags`
---
-ALTER TABLE `tags`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
